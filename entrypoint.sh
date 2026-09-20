421467e104de639e5906bd9be3abd568add7e76b
Sriram <ssrriirraamm444@gmail.com>
2026-09-20 16:07:40 +0530

    fix: allow H2 to create database on first connection via -ifNotExists

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Starting H2 server (TCP mode)..."
-java -cp /opt/h2.jar org.h2.tools.Server -tcp -tcpAllowOthers -tcpPort 9092 -baseDir /opt/srirammart/data &
+java -cp /opt/h2.jar org.h2.tools.Server -tcp -tcpAllowOthers -tcpPort 9092 -ifNotExists -baseDir /opt/srirammart/data &
 
 sleep 3
 
